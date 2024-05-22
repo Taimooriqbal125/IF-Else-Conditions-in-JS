4b475f45e6b732939e6c234cd392f9e1ab5905ca
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-05-22 02:57:03 -0700

    IF Else IN JS

---
+++ script.js
@@ -0,0 +1,9 @@
+// Use of block is same like use of Attribute in CSS
+// How to apply Block by { Name of Action }
+// ( False / True   ) before the { Name of Action }
+
+//  Example :
+var True = 0;
+ if (True == 0 ){
+    console.log("hello")
+ }
