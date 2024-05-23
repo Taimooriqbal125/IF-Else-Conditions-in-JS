2ae9c06e33c80b5a07362e0f0ac23ce55deec0af
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-05-23 02:49:41 -0700

    IF ELSE STATEMENT

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ var True = 0;
  if (True == 0 ){
     console.log("hello")
  }
+//  In this example Name of action is IF . and in (  ) condition is true it mean run
+
+// EXAMPLE 001
+// If time is less than 10:00, create a "Good morning" greeting, if not, but time is less than 20:00, 
+// create a "Good day" greeting, otherwise a "Good evening":
+ 
+var time = 8;
+if (time < 10){
+   console.log("Good Morning")
+}
+else if (time < 20){
+   console.log("Good Day")
+}
+else {
+   console.log("Good evening")
+}
